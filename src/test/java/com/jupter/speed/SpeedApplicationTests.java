cf04fd8f61545f04de6bb8fcdbe7e35d90c056e9
mayconUllrich <maycon123@gmail.com>
2025-03-12 23:52:20 -0300

    Commig

---
+++ src/test/java/com/jupter/speed/SpeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jupter.speed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
